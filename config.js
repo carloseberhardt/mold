55c346da36fe234103a2a85116805f3f16d4ed2f
Carlos Eberhardt <carlos.eberhardt@gmail.com>
2015-09-04 01:21:50 -0500

    got things working, roughly

--- config.js
+++ config.js
@@ -1,8 +1,22 @@
 var config = {}
+// since this is js, we could use apigee-access here if we had a need
+// and keep index.js very generic
+// but static config is fine for a start
 
+// config.target
 config.target = {
-  href: 'the url',
-  options: {doc: 'standard request options'}
+  options: {
+    method: 'GET',
+    url: 'http://api.lo5.at/surepark',
+    headers: {
+      'User-Agent': 'mold request'
+    }
+  }
+}
+
+config.contentTypes = {
+  'text/html': '<html><head></head><body><ul>{{#parkinginfo.item}}<li>{{countAreaName}} is {{percentFull}}% full</li>{{/parkinginfo.item}}</ul></body></html>',
+  'application/json': '{{#each parkinginfo.item}}{{@key}}:{{this.capacity}}{{/each}}'
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var http = require('http')
 var request = require('request')
 var config = require('./config')
+var handlebars = require('handlebars')
 
 var srv = http.createServer(function (req, res) {
-  res.end('OK')
+  var accept = req.headers.accept.split(',')
+  console.log('accept header: ' + JSON.stringify(accept))
+  // TODO: this needs to be smarter
+  // what this does is get browsers to work when we have a text/html
+  // content type defined.. maybe it's OK?
+  accept = accept[0]
+  if (!config.contentTypes.hasOwnProperty(accept)) {
+    res.writeHead(406, {'Content-Type': 'application/json'})
+    var r = {}
+    r.message = 'Unsupported Accept header.'
+    r.acceptable = Object.getOwnPropertyNames(config.contentTypes)
+    res.end(JSON.stringify(r))
+  } else {
+    var template = handlebars.compile(config.contentTypes[accept])
+    request(config.target.options, function (error, response, body) {
+      if (error) {
+        res.end('error! error!')
+      } else {
+        // TODO: should probably check statuscode.. fix this
+        var result = template(JSON.parse(body))
+        // console.log(result)
+        res.setHeader('Content-Type', req.headers.accept)
+        res.end(result)
+      }
+    })
+  }
 })
 
 srv.listen(8128, function () {
